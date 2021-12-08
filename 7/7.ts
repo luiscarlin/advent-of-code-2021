e069f15c6eaec2089fed8562c31106b2f578da89
Luis Carlin <luiscarlin@gmail.com>
2021-12-07 22:13:09 -0500

    day 7 part 2 done

---
+++ 7/7.ts
@@ -0,0 +1,40 @@
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+
+function part1() {
+  let line = readFileSync('./7/7.in', 'utf8').split('\n')[0].split(',').map(Number);
+
+  const sortedLine = _.sortBy(line);
+
+  const mid = sortedLine[sortedLine.length / 2];
+  let answer = 0;
+
+  for (let num of sortedLine) {
+    answer += Math.abs(num - mid);
+  }
+
+  return answer;
+}
+
+function part2() {
+  let line = readFileSync('./7/7.in', 'utf8').split('\n')[0].split(',').map(Number);
+
+  const sortedLine = _.sortBy(line);
+
+  const highestLocation = sortedLine[sortedLine.length - 1];
+
+  const allFuelCosts = [];
+
+  for (let meet of sortedLine) {
+    const fuelCost = _.sum(
+      sortedLine.map((position) => (Math.abs(position - meet) * (1 + Math.abs(position - meet))) / 2),
+    );
+
+    allFuelCosts.push(fuelCost);
+  }
+
+  return _.min(allFuelCosts);
+}
+
+console.log('part 1', part1());
+console.log('part 2', part2());
