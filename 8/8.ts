803c97f77bed2d317eb145e1823aea91501eef1d
Luis Carlin <luiscarlin@gmail.com>
2021-12-08 22:58:08 -0500

    day 8 part 2 done

--- 8/8.ts
+++ 8/8.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from 'fs';
-
+import _ from 'lodash';
 function part1() {
   let lines = readFileSync('./8/8.in', 'utf8')
     .split('\n')
@@ -17,22 +17,44 @@ function part1() {
   return count;
 }
 
+function includes(a, b) {
+  const set = new Set([...a]);
+  return [...b].every((x) => set.has(x));
+}
+
 function part2() {
   let lines = readFileSync('./8/8.in', 'utf8')
     .split('\n')
-    .map((line) => line.split(' | ')[1])
-    .map((line) => line.split(' '));
+    .map((line) => line.split(' | '))
+    .map((p) => p.map((a) => a.split(' ').map((c) => _.sortBy(c).join(''))));
 
-  let count = 0;
+  let total = 0;
 
-  lines.flat().forEach((segment) => {
-    if ([2, 4, 3, 7].includes(segment.length)) {
-      count++;
-    }
-  });
+  for (let [signal, output] of lines) {
+    const matches = {
+      1: signal.find((x) => x.length === 2),
+      4: signal.find((x) => x.length === 4),
+      7: signal.find((x) => x.length === 3),
+      8: signal.find((x) => x.length === 7),
+    };
 
-  return count;
+    matches[6] = signal.find((x) => x.length === 6 && !includes(x, matches[1]));
+    matches[9] = signal.find((x) => x.length === 6 && x !== matches[6] && includes(x, matches[4]));
+    matches[0] = signal.find((x) => x.length === 6 && x !== matches[6] && x !== matches[9]);
+
+    matches[3] = signal.find((x) => x.length === 5 && includes(x, matches[1]));
+    matches[5] = signal.find((x) => x.length === 5 && x !== matches[3] && includes(matches[6], x));
+    matches[2] = signal.find((x) => x.length === 5 && x !== matches[3] && x !== matches[5]);
+
+    const translationTable = Object.fromEntries(Object.entries(matches).map((x) => x.reverse()));
+
+    const translated = Number(output.map((signal) => translationTable[signal]).join(''));
+
+    total += translated;
+  }
+
+  return total;
 }
 
-// console.log('part 1', part1());
+console.log('part 1', part1());
 console.log('part 1', part2());
